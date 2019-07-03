c1a7ff80d1ae6be5fdc3a38948596da0a358bae1
agaviliHIP <52498557+agaviliHIP@users.noreply.github.com>
2019-07-03 17:09:03 -0400

    Add files via upload

---
+++ mapdata.js
@@ -0,0 +1,1109 @@
+var simplemaps_worldmap_mapdata={
+  main_settings: {
+   //General settings
+    width: "responsive", //'700' or 'responsive'
+    background_color: "#e8eaed",
+    background_transparent: "yes",
+    border_color: "#ffffff",
+    popups: "detect",
+    
+    //State defaults
+    state_description: "State description",
+    state_color: "#88A4BC",
+    state_hover_color: "#3B729F",
+    state_url: "http://simplemaps.com",
+    border_size: 1.5,
+    all_states_inactive: "no",
+    all_states_zoomable: "no",
+    
+    //Location defaults
+    location_description: "Location description",
+    location_color: "#FF0067",
+    location_opacity: 0.8,
+    location_hover_opacity: 1,
+    location_url: "",
+    location_size: 25,
+    location_type: "square",
+    location_image_source: "frog.png",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+    
+    //Label defaults
+    label_color: "#d5ddec",
+    label_hover_color: "#d5ddec",
+    label_size: 22,
+    label_font: "Arial",
+    hide_labels: "no",
+   
+    //Zoom settings
+    zoom: "yes",
+    back_image: "no",
+    initial_back: "no",
+    initial_zoom: -1,
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+    
+    //Popup settings
+    popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+    
+    //Advanced settings
+    div: "map",
+    auto_load: "yes",
+    url_new_tab: "yes",
+    images_directory: "default",
+    fade_time: 0.1,
+    link_text: "View Website",
+    state_image_url: "",
+    state_image_position: "",
+    location_image_url: ""
+  },
+  state_specific: {
+    AF: {
+      name: "Afghanistan",
+      color: "#481f7a",
+      description: "Covered"
+    },
+    AO: {
+      name: "Angola",
+      color: "#224c69",
+      description: "Covered"
+    },
+    AL: {
+      name: "Albania"
+    },
+    AE: {
+      name: "United Arab Emirates",
+      color: "#de5a1b",
+      description: "Covered"
+    },
+    AR: {
+      name: "Argentina",
+      color: "#0e8212",
+      description: "Covered"
+    },
+    AM: {
+      name: "Armenia"
+    },
+    AU: {
+      name: "Australia"
+    },
+    AT: {
+      name: "Austria"
+    },
+    AZ: {
+      name: "Azerbaijan"
+    },
+    BI: {
+      name: "Burundi",
+      color: "#224c69",
+      description: "Covered"
+    },
+    BE: {
+      name: "Belgium"
+    },
+    BJ: {
+      name: "Benin",
+      color: "#224c69",
+      description: "Covered"
+    },
+    BF: {
+      name: "Burkina Faso",
+      color: "#224c69",
+      description: "Covered"
+    },
+    BD: {
+      name: "Bangladesh",
+      color: "#481f7a",
+      description: "Covered"
+    },
+    BG: {
+      name: "Bulgaria"
+    },
+    BH: {
+      name: "Bahrain"
+    },
+    BA: {
+      name: "Bosnia and Herzegovina"
+    },
+    BY: {
+      name: "Belarus"
+    },
+    BZ: {
+      name: "Belize",
+      color: "#0e8212",
+      description: "Covered"
+    },
+    BO: {
+      name: "Bolivia",
+      color: "#0e8212",
+      description: "Covered"
+    },
+    BR: {
+      name: "Brazil",
+      color: "#0e8212",
+      description: "Covered"
+    },
+    BN: {
+      name: "Brunei Darussalam",
+      color: "#e3b818",
+      description: "Covered"
+    },
+    BT: {
+      name: "Bhutan"
+    },
+    BW: {
+      name: "Botswana",
+      color: "#224c69",
+      description: "Covered"
+    },
+    CF: {
+      name: "Central African Republic",
+      color: "#224c69"
+    },
+    CA: {
+      name: "Canada",
+      color: "#931836",
+      description: "Covered"
+    },
+    CH: {
+      name: "Switzerland"
+    },
+    CL: {
+      name: "Chile",
+      color: "#0e8212",
+      description: "Covered"
+    },
+    CN: {
+      name: "China"
+    },
+    CI: {
+      name: "Côte d'Ivoire",
+      color: "#224c69",
+      description: "Covered"
+    },
+    CM: {
+      name: "Cameroon",
+      color: "#224c69",
+      description: "Covered"
+    },
+    CD: {
+      name: "Democratic Republic of the Congo",
+      color: "#224c69",
+      description: "Covered"
+    },
+    CG: {
+      name: "Republic of Congo",
+      color: "#224c69",
+      description: "Covered"
+    },
+    CO: {
+      name: "Colombia",
+      color: "#0e8212",
+      description: "Covered"
+    },
+    CR: {
+      name: "Costa Rica",
+      color: "#0e8212",
+      description: "Covered"
+    },
+    CU: {
+      name: "Cuba"
+    },
+    CZ: {
+      name: "Czech Republic"
+    },
+    DE: {
+      name: "Germany"
+    },
+    DJ: {
+      name: "Djibouti",
+      color: "#de5a1b",
+      description: "Covered"
+    },
+    DK: {
+      name: "Denmark"
+    },
+    DO: {
+      name: "Dominican Republic",
+      color: "#0e8212",
+      description: "Covered"
+    },
+    DZ: {
+      name: "Algeria",
+      color: "#de5a1b",
+      description: "Covered"
+    },
+    EC: {
+      name: "Ecuador",
+      color: "#0e8212",
+      description: "Covered"
+    },
+    EG: {
+      name: "Egypt",
+      color: "#de5a1b",
+      description: "Covered"
+    },
+    ER: {
+      name: "Eritrea"
+    },
+    EE: {
+      name: "Estonia"
+    },
+    ET: {
+      name: "Ethiopia",
+      color: "#224c69",
+      description: "Covered"
+    },
+    FI: {
+      name: "Finland"
+    },
+    FJ: {
+      name: "Fiji"
+    },
+    GA: {
+      name: "Gabon",
+      color: "#224c69",
+      description: "Covered"
+    },
+    GB: {
+      name: "United Kingdom"
+    },
+    GE: {
+      name: "Georgia"
+    },
+    GH: {
+      name: "Ghana",
+      color: "#224c69",
+      description: "Covered"
+    },
+    GN: {
+      name: "Guinea",
+      color: "#224c69",
+      description: "Covered"
+    },
+    GM: {
+      name: "The Gambia",
+      color: "#224c69",
+      description: "Covered"
+    },
+    GW: {
+      name: "Guinea-Bissau",
+      color: "#224c69",
+      description: "Covered"
+    },
+    GQ: {
+      name: "Equatorial Guinea",
+      color: "#224c69",
+      description: "Covered"
+    },
+    GR: {
+      name: "Greece"
+    },
+    GL: {
+      name: "Greenland"
+    },
+    GT: {
+      name: "Guatemala",
+      color: "#0e8212",
+      description: "Covered"
+    },
+    GY: {
+      name: "Guyana",
+      color: "#0e8212",
+      description: "Covered"
+    },
+    HN: {
+      name: "Honduras",
+      color: "#0e8212",
+      description: "Covered"
+    },
+    HR: {
+      name: "Croatia"
+    },
+    HT: {
+      name: "Haiti"
+    },
+    HU: {
+      name: "Hungary"
+    },
+    ID: {
+      name: "Indonesia",
+      color: "#e3b818",
+      description: "Covered"
+    },
+    IN: {
+      name: "India",
+      color: "#481f7a",
+      description: "Covered"
+    },
+    IE: {
+      name: "Ireland"
+    },
+    IR: {
+      name: "Iran"
+    },
+    IQ: {
+      name: "Iraq"
+    },
+    IS: {
+      name: "Iceland"
+    },
+    IL: {
+      name: "Israel"
+    },
+    IT: {
+      name: "Italy"
+    },
+    JM: {
+      name: "Jamaica"
+    },
+    JO: {
+      name: "Jordan",
+      color: "#de5a1b",
+      description: "Covered"
+    },
+    JP: {
+      name: "Japan"
+    },
+    KZ: {
+      name: "Kazakhstan",
+      color: "#c5108f",
+      description: "Covered"
+    },
+    KE: {
+      name: "Kenya",
+      color: "#224c69",
+      description: "Covered"
+    },
+    KG: {
+      name: "Kyrgyzstan"
+    },
+    KH: {
+      name: "Cambodia",
+      color: "#e3b818",
+      description: "Covered"
+    },
+    KR: {
+      name: "Republic of Korea"
+    },
+    XK: {
+      name: "Kosovo"
+    },
+    KW: {
+      name: "Kuwait"
+    },
+    LA: {
+      name: "Lao PDR",
+      color: "#e3b818",
+      description: "Covered"
+    },
+    LB: {
+      name: "Lebanon"
+    },
+    LR: {
+      name: "Liberia",
+      color: "#224c69",
+      description: "Covered"
+    },
+    LY: {
+      name: "Libya",
+      color: "#de5a1b",
+      description: "Covered"
+    },
+    LK: {
+      name: "Sri Lanka",
+      color: "#481f7a",
+      description: "Covered"
+    },
+    LS: {
+      name: "Lesotho",
+      color: "#224c69",
+      description: "Covered"
+    },
+    LT: {
+      name: "Lithuania"
+    },
+    LU: {
+      name: "Luxembourg"
+    },
+    LV: {
+      name: "Latvia"
+    },
+    MA: {
+      name: "Morocco",
+      color: "#de5a1b",
+      description: "Covered"
+    },
+    MD: {
+      name: "Moldova"
+    },
+    MG: {
+      name: "Madagascar",
+      color: "#224c69",
+      description: "Covered"
+    },
+    MX: {
+      name: "Mexico",
+      color: "#0e8212",
+      description: "Covered"
+    },
+    MK: {
+      name: "Macedonia"
+    },
+    ML: {
+      name: "Mali",
+      color: "#224c69",
+      description: "Covered"
+    },
+    MM: {
+      name: "Myanmar",
+      color: "#e3b818",
+      description: "Covered"
+    },
+    ME: {
+      name: "Montenegro"
+    },
+    MN: {
+      name: "Mongolia",
+      color: "#e3b818",
+      description: "Covered"
+    },
+    MZ: {
+      name: "Mozambique",
+      color: "#224c69",
+      description: "Covered"
+    },
+    MR: {
+      name: "Mauritania",
+      color: "#224c69",
+      description: "Covered"
+    },
+    MW: {
+      name: "Malawi",
+      color: "#224c69",
+      description: "Covered"
+    },
+    MY: {
+      name: "Malaysia",
+      color: "#e3b818",
+      description: "Covered"
+    },
+    NA: {
+      name: "Namibia",
+      color: "#224c69",
+      description: "Covered"
+    },
+    NE: {
+      name: "Niger",
+      color: "#224c69",
+      description: "Covered"
+    },
+    NG: {
+      name: "Nigeria",
+      color: "#224c69",
+      description: "Covered"
+    },
+    NI: {
+      name: "Nicaragua",
+      color: "#0e8212",
+      description: "Covered"
+    },
+    NL: {
+      name: "Netherlands"
+    },
+    NO: {
+      name: "Norway"
+    },
+    NP: {
+      name: "Nepal",
+      color: "#481f7a",
+      description: "Covered"
+    },
+    NZ: {
+      name: "New Zealand"
+    },
+    OM: {
+      name: "Oman",
+      color: "#de5a1b",
+      description: "Covered"
+    },
+    PK: {
+      name: "Pakistan",
+      color: "#481f7a",
+      description: "Covered"
+    },
+    PA: {
+      name: "Panama",
+      color: "#0e8212",
+      description: "Covered"
+    },
+    PE: {
+      name: "Peru",
+      color: "#0e8212",
+      description: "Covered"
+    },
+    PH: {
+      name: "Philippines",
+      color: "#e3b818",
+      description: "Covered"
+    },
+    PG: {
+      name: "Papua New Guinea",
+      color: "#e3b818",
+      description: "Covered"
+    },
+    PL: {
+      name: "Poland"
+    },
+    KP: {
+      name: "Dem. Rep. Korea"
+    },
+    PT: {
+      name: "Portugal"
+    },
+    PY: {
+      name: "Paraguay",
+      color: "#0e8212",
+      description: "Covered"
+    },
+    PS: {
+      name: "Palestine"
+    },
+    QA: {
+      name: "Qatar"
+    },
+    RO: {
+      name: "Romania"
+    },
+    RU: {
+      name: "Russia"
+    },
+    RW: {
+      name: "Rwanda",
+      color: "#224c69",
+      description: "Covered"
+    },
+    EH: {
+      name: "Western Sahara"
+    },
+    SA: {
+      name: "Saudi Arabia",
+      color: "#de5a1b",
+      description: "Covered"
+    },
+    SD: {
+      name: "Sudan",
+      color: "#224c69",
+      description: "Covered"
+    },
+    SS: {
+      name: "South Sudan",
+      color: "#224c69",
+      description: "Covered"
+    },
+    SN: {
+      name: "Senegal",
+      color: "#224c69",
+      description: "Covered"
+    },
+    SL: {
+      name: "Sierra Leone",
+      color: "#224c69",
+      description: "Covered"
+    },
+    SV: {
+      name: "El Salvador",
+      color: "#0e8212",
+      description: "Covered"
+    },
+    RS: {
+      name: "Serbia"
+    },
+    SR: {
+      name: "Suriname"
+    },
+    SK: {
+      name: "Slovakia"
+    },
+    SI: {
+      name: "Slovenia"
+    },
+    SE: {
+      name: "Sweden"
+    },
+    SZ: {
+      name: "Swaziland",
+      color: "#224c69",
+      description: "Covered"
+    },
+    SY: {
+      name: "Syria"
+    },
+    TD: {
+      name: "Chad",
+      color: "#224c69",
+      description: "Covered"
+    },
+    TG: {
+      name: "Togo",
+      color: "#224c69",
+      description: "Covered"
+    },
+    TH: {
+      name: "Thailand",
+      color: "#e3b818",
+      description: "Covered"
+    },
+    TJ: {
+      name: "Tajikistan"
+    },
+    TM: {
+      name: "Turkmenistan"
+    },
+    TL: {
+      name: "Timor-Leste"
+    },
+    TN: {
+      name: "Tunisia",
+      color: "#de5a1b",
+      description: "Covered"
+    },
+    TR: {
+      name: "Turkey",
+      color: "#c5108f",
+      description: "Covered"
+    },
+    TW: {
+      name: "Taiwan",
+      color: "#e3b818",
+      description: "Covered"
+    },
+    TZ: {
+      name: "Tanzania",
+      color: "#224c69",
+      description: "Covered"
+    },
+    UG: {
+      name: "Uganda",
+      color: "#224c69",
+      description: "Covered"
+    },
+    UA: {
+      name: "Ukraine",
+      color: "#c5108f",
+      description: "Covered"
+    },
+    UY: {
+      name: "Uruguay",
+      color: "#0e8212",
+      description: "Covered"
+    },
+    US: {
+      name: "United States",
+      color: "#931836",
+      description: "Covered"
+    },
+    UZ: {
+      name: "Uzbekistan",
+      color: "#c5108f",
+      description: "Covered"
+    },
+    VE: {
+      name: "Venezuela",
+      color: "#0e8212",
+      description: "Covered"
+    },
+    VN: {
+      name: "Vietnam"
+    },
+    VU: {
+      name: "Vanuatu"
+    },
+    YE: {
+      name: "Yemen"
+    },
+    ZA: {
+      name: "South Africa",
+      color: "#224c69",
+      description: "Covered"
+    },
+    ZM: {
+      name: "Zambia",
+      color: "#224c69",
+      description: "Covered"
+    },
+    ZW: {
+      name: "Zimbabwe",
+      color: "#224c69",
+      description: "Covered"
+    },
+    SO: {
+      name: "Somalia",
+      color: "#224c69",
+      description: "Covered"
+    },
+    GF: {
+      name: "France"
+    },
+    FR: {
+      name: "France"
+    },
+    ES: {
+      name: "Spain"
+    },
+    AW: {
+      name: "Aruba"
+    },
+    AI: {
+      name: "Anguilla"
+    },
+    AD: {
+      name: "Andorra"
+    },
+    AG: {
+      name: "Antigua and Barbuda"
+    },
+    BS: {
+      name: "Bahamas"
+    },
+    BM: {
+      name: "Bermuda"
+    },
+    BB: {
+      name: "Barbados"
+    },
+    KM: {
+      name: "Comoros"
+    },
+    CV: {
+      name: "Cape Verde",
+      color: "#224c69",
+      description: "Covered"
+    },
+    KY: {
+      name: "Cayman Islands"
+    },
+    DM: {
+      name: "Dominica"
+    },
+    FK: {
+      name: "Falkland Islands"
+    },
+    FO: {
+      name: "Faeroe Islands"
+    },
+    GD: {
+      name: "Grenada"
+    },
+    HK: {
+      name: "Hong Kong"
+    },
+    KN: {
+      name: "Saint Kitts and Nevis"
+    },
+    LC: {
+      name: "Saint Lucia"
+    },
+    LI: {
+      name: "Liechtenstein"
+    },
+    MF: {
+      name: "Saint Martin (French)"
+    },
+    MV: {
+      name: "Maldives"
+    },
+    MT: {
+      name: "Malta"
+    },
+    MS: {
+      name: "Montserrat"
+    },
+    MU: {
+      name: "Mauritius",
+      color: "#224c69",
+      description: "Covered"
+    },
+    NC: {
+      name: "New Caledonia"
+    },
+    NR: {
+      name: "Nauru"
+    },
+    PN: {
+      name: "Pitcairn Islands"
+    },
+    PR: {
+      name: "Puerto Rico"
+    },
+    PF: {
+      name: "French Polynesia"
+    },
+    SG: {
+      name: "Singapore"
+    },
+    SB: {
+      name: "Solomon Islands"
+    },
+    ST: {
+      name: "São Tomé and Principe"
+    },
+    SX: {
+      name: "Saint Martin (Dutch)"
+    },
+    SC: {
+      name: "Seychelles"
+    },
+    TC: {
+      name: "Turks and Caicos Islands"
+    },
+    TO: {
+      name: "Tonga"
+    },
+    TT: {
+      name: "Trinidad and Tobago"
+    },
+    VC: {
+      name: "Saint Vincent and the Grenadines"
+    },
+    VG: {
+      name: "British Virgin Islands"
+    },
+    VI: {
+      name: "United States Virgin Islands"
+    },
+    CY: {
+      name: "Cyprus"
+    },
+    RE: {
+      name: "Reunion (France)"
+    },
+    YT: {
+      name: "Mayotte (France)"
+    },
+    MQ: {
+      name: "Martinique (France)"
+    },
+    GP: {
+      name: "Guadeloupe (France)"
+    },
+    CW: {
+      name: "Curaco (Netherlands)"
+    },
+    IC: {
+      name: "Canary Islands (Spain)"
+    }
+  },
+  locations: {},
+  labels: {},
+  regions: {
+    "0": {
+      name: "North America",
+      states: [
+        "CA",
+        "US",
+        "GL",
+        "BM"
+      ]
+    },
+    "1": {
+      name: ":atin America",
+      states: [
+        "EC",
+        "AR",
+        "VE",
+        "BR",
+        "CO",
+        "BO",
+        "PE",
+        "BZ",
+        "CL",
+        "CR",
+        "CU",
+        "DO",
+        "SV",
+        "GT",
+        "GY",
+        "GF",
+        "HN",
+        "NI",
+        "PA",
+        "PY",
+        "PR",
+        "SR",
+        "UY",
+        "JM",
+        "HT",
+        "BS",
+        "FK",
+        "AI",
+        "AG",
+        "AW",
+        "BB",
+        "VG",
+        "KY",
+        "DM",
+        "MQ",
+        "LC",
+        "VC",
+        "GD",
+        "GP",
+        "MS",
+        "TC",
+        "SX",
+        "MF",
+        "KN",
+        "CW",
+        "MX"
+      ]
+    },
+    "2": {
+      states: [
+        "KH",
+        "ID",
+        "MM",
+        "MY",
+        "MN",
+        "BN",
+        "LA",
+        "PG",
+        "PH",
+        "TH",
+        "TW"
+      ],
+      name: "East and Pacific Asia"
+    },
+    "3": {
+      name: "Sub-Saharan Africa",
+      states: [
+        "QA",
+        "BH",
+        "AE",
+        "SY",
+        "KW",
+        "AZ",
+        "IR",
+        "IQ",
+        "IL",
+        "PS",
+        "LB",
+        "YE",
+        "TJ",
+        "TM",
+        "UZ",
+        "KG",
+        "NE",
+        "AO",
+        "GA",
+        "DZ",
+        "LY",
+        "CG",
+        "GQ",
+        "BJ",
+        "BW",
+        "BF",
+        "BI",
+        "CM",
+        "CF",
+        "TD",
+        "CI",
+        "CD",
+        "DJ",
+        "ET",
+        "GM",
+        "GH",
+        "GN",
+        "GW",
+        "KE",
+        "LS",
+        "LR",
+        "MG",
+        "MW",
+        "ML",
+        "MR",
+        "MZ",
+        "NA",
+        "NG",
+        "ER",
+        "RW",
+        "SN",
+        "SL",
+        "SO",
+        "ZA",
+        "SD",
+        "SS",
+        "SZ",
+        "TZ",
+        "TG",
+        "UG",
+        "EH",
+        "ZM",
+        "ZW",
+        "RE",
+        "KM",
+        "SC",
+        "MU",
+        "CV",
+        "IC",
+        "ST",
+        "YT"
+      ]
+    },
+    "4": {
+      name: "South Asia",
+      states: [
+        "SG",
+        "IN",
+        "AU",
+        "NP",
+        "NZ",
+        "JP",
+        "VN",
+        "LK",
+        "SB",
+        "FJ",
+        "BD",
+        "BT",
+        "KP",
+        "KR",
+        "CN",
+        "MV",
+        "NC",
+        "VU",
+        "NR",
+        "PK",
+        "AF"
+      ]
+    },
+    "5": {
+      name: "North Asia",
+      states: [
+        "RU",
+        "KZ"
+      ]
+    },
+    "6": {
+      states: [
+        "JO",
+        "TN",
+        "EG",
+        "OM",
+        "MA",
+        "SA"
+      ],
+      name: "MENA"
+    }
+  }
+};
